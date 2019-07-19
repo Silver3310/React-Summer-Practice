c797e62e7e89f7c9f1c0d69772cfaeeedf23caa4
Silver3310 <epifanov998@mail.ru>
2019-07-19 14:21:36 +0700

    Add routing
    And check how it works by creating other components (maybe they will be deleted in the future)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
-  );
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import Header from './components/layout/Header';
+
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+        <Router>
+          <div className="App">
+            <div className="container">
+              <Header />
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </div>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
