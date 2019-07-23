414601327a007e67f5ddcd531bff8d0695864596
Silver3310 <epifanov998@mail.ru>
2019-07-23 13:42:45 +0700

    Refactor the existing code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
 
-import About from '../../routes/about/About';
-import Home from '../../routes/home/Home';
-import Header from './header/Header';
+import About from '../../routes/about';
+import Home from '../../routes/home';
+import Header from './header';
 
 
 class App extends React.Component {
@@ -16,7 +15,6 @@ class App extends React.Component {
           <div className="App">
             <div className="container">
               <Header />
-              <Button primary>Just a button, nothing fancy</Button>
               <Route exact path="/" component={Home} />
               <Route path="/about" component={About} />
             </div>
